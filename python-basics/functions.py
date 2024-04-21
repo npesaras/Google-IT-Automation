09bfaeeef9edd170ef8e55ed0fc1b99a83553746
Nilmar Pesaras <148970089+npesaras@users.noreply.github.com>
2024-04-21 20:05:09 +0800

    Update

---
+++ python-basics/functions.py
@@ -0,0 +1,13 @@
+def greeting(name):
+    print("Welcome, " + name)
+    
+greeting("Kay")
+greeting("Cameron")
+
+
+def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part of " + department)
+    
+greeting("Blake", "Software engineering")
+greeting("Ellis", "Software engineering")
